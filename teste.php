fb81587ecff9936195f1c7278197fc28e671a990
Nicolas Antonio Smaniotto <49732409+Nicolassmaniotto@users.noreply.github.com>
2021-08-19 16:39:21 -0300

    a

--- teste.php
+++ teste.php
@@ -1,13 +1,18 @@
 <?php
 require_once  "vendor/autoload.php";
 use Data\Db\getDatadb;
+// $conn = (object)[
+//     'username'=>'', //your username for db access
+//     'servername'=>'',//your servel local exmple 'localhost'
+//     'password'=>'', //your password db
+//     'database'=>'' //your database example 'test'
+// ];
 $conn = (object)[
-    'username'=>'', //your username for db access
-    'servername'=>'',//your servel local exmple 'localhost'
-    'password'=>'', //your password db
-    'database'=>'' //your database example 'test'
+    'username'=>'root',
+    'servername'=>'localhost',
+    'password'=>'roottoor',
+    'database'=>'mdb'
 ];
-
 $getDB = new getDatadb;
 $getDB->conn = $conn;
 echo $getDB->getCTimestamp(); // exemplo get current_timestamp db server
