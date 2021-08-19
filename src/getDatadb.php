732b43250f384c47d56d09b8c46a58a1797d3931
Nicolas Antonio Smaniotto <49732409+Nicolassmaniotto@users.noreply.github.com>
2021-08-19 17:10:01 -0300

    a

--- src/getDatadb.php
+++ src/getDatadb.php
@@ -4,27 +4,32 @@
 use mysqli;
 
 class getDatadb{
-        // static function connect(){
-        //     $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
-        // }
-        static function getDb($option){
-            
-        }
-        public function getCTimestamp(){//get current_timestamp of db
-            if(!isset($this->conn)){
-                $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+        public function connect($option){
+            if(!isset($option->conn)){
+                $conn = mysqli_connect($option->servername, $option->username, $option->password, $option->database);
             }else{
-                $dataConn = $this->conn;
+                $dataConn = $option->conn;
                 $conn = mysqli_connect($dataConn->servername, $dataConn->username, $dataConn->password, $dataConn->database);
 
             }
+            // $conn = mysqli_connect($option->servername, $option->username, $option->password, $option->database);
+            return $conn;
+        }
+        static function getDb($option){
+            if(isset($option)){
+
+            }
+        }
+        public function getCTimestamp(){//get current_timestamp of db
+
+            $conn = $this->connect($this->conn);
             $sql = "SELECT current_timestamp;";
             $result = mysqli_query($conn, $sql);
             $row   = mysqli_fetch_row($result);
             mysqli_close($conn);
             return $row[0];
 
-            //$ssfullname = $row['ssfullname'];
+            $ssfullname = $row['ssfullname'];
         }
         public function insertData(){
             $this->data;
